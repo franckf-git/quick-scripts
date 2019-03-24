e743b6ab489654cd4dffc962515e87a884749cf3
efydd <efydd.artio@tuta.io>
2019-03-24 08:52:01 +0100

    update_config_from_repo.sh fix config

--- update_config_from_repo.sh
+++ update_config_from_repo.sh
@@ -7,41 +7,53 @@ COLOR_OFF="\033[0m"
 
 echo -e "${GREEN} # Download my config files from gitlab and put them on ${COLOR_OFF}"
 
-ranger --copy-config=all
 cd ~/Downloads
+mkdir ~/Downloads/OLDconfig
+
 wget https://framagit.org/efydd/config/-/archive/master/config-master.tar
-tar xvf config-master.tar
+tar xf config-master.tar
 
 cd config-master
 for dotfile in "
 .Xdefaults
 .bash.command-not-found
 .bashrc
+.check_repo.sh 
 .gitconfig
 .gtkrc-2.0"
 do
-    mv --force $dotfile ~/
+    cp ~/$dotfile ~/Downloads/OLDconfig/
+    cp $dotfile ~/
 done
 
 for dotfolder in "
-.cheat/
 .conky/
-.rtfm/
+.noob/
 .sec/
-.themes/
-.tldr/"
+.surf/
+.themes/"
 do
-    mv --force $dotfolder/* ~/$dotfolder/
+    cp -r ~/$dotfolder ~/Downloads/OLDconfig/
+    cp -r $dotfolder ~/
 done
 
-cd ~/Downloads/config-master
+cd .config
 for dotconfig in "
+dunst
 i3
 i3status
 nvim
-ranger"
+rofi"
 do
-    mv .config/$dotconfig/* ~/.config/$dotconfig/
+    cp -r ~/.config/$dotconfig ~/Downloads/OLDconfig/.config/
+    cp -r $dotconfig ~/.config/
 done
+
+cp -r ~/.config/ranger ~/Downloads/OLDconfig/
+rm -R ~/.config/ranger
+ranger --copy-config=all
+cp ranger/* ~/.config/ranger/
+
+i3-msg restart
 
 echo -e "[${GREEN} DONE ${COLOR_OFF}]"
