5f607b2026ad959c7b5291ff1a064ef8c0e6cd0a
efydd <efydd.artio@tuta.io>
2019-05-12 10:50:01 +0200

    colors_tests/color.sh longer text for testing colors

--- colors_tests/color.sh
+++ colors_tests/color.sh
@@ -9,7 +9,7 @@
 #   colors (default + 8 escapes).
 #
 
-T='TiP'   # The test text
+T='  Hello World !'   # The test text
 
 echo -e "\n                 40m     41m     42m     43m\
      44m     45m     46m     47m";
