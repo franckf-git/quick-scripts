448fb92cd6a08c0db927b1d544937cd79e1cabd1
franckf <efydd.artio@tuta.io>
2020-07-15 09:41:13 +0200

    dossier zip d images transformer en pdf

---
+++ medias/scanszip_vers_pdf.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+# dependances : ImageMagick et prename
+dossier=$1
+cd $dossier
+nomdossier=$(echo $dossier | cut -d"/" -f1)
+
+# renommage
+renommage (){
+    for zip in $(ls)
+    do
+        mv $zip $(echo $zip | iconv --from-code=UTF-8 --to-code=ASCII//TRANSLIT)
+    done
+
+    prename 'y/A-Z\ /a-z-/' *
+    prename 's/\[//g' *
+    prename 's/[^a-z0-9-.]/-/g' *
+}
+
+renommage
+
+mkdir faits
+
+# dezipper tous les fichiers
+for zip in $(ls | egrep '*.zip')
+do
+    mv $zip faits/
+    cd faits/
+    # todo verifier si cest un dossier
+    unzip $zip
+    nom=$(basename $zip .zip)
+    mv $zip ../../
+    renommage
+
+    for image in $(ls)
+    do
+        mv $image ../"$nom"_"$image"
+    done
+
+    cd ../
+done
+
+convert *.* "$nomdossier".pdf
+mv "$nomdossier".pdf ../
+
