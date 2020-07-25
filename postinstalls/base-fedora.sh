26ad9d9fe6378e5ef23d3d8b4d443ecdd3a54a62
franckf <efydd.artio@tuta.io>
2020-07-25 09:31:21 +0200

    disable print server

--- postinstalls/base-fedora.sh
+++ postinstalls/base-fedora.sh
@@ -19,6 +19,7 @@ echo 'keepcache=true' >> /etc/dnf/dnf.conf
 #systemctl disable dnf-makecache.service
 #systemctl disable dnf-makecache.timer
 ############ Disable unused services
+systemctl list-units | grep service >> list-services
 systemctl disable chronyd.service
 systemctl disable cups.service
 systemctl disable ModemManager.service
@@ -28,6 +29,7 @@ systemctl disable nfs-convert.service
 systemctl disable qemu-guest-agent.service
 systemctl disable spice-vdagentd.socket
 systemctl disable vboxservice.service
+systemctl disable cups.service
 systemctl disable rtkit-daemon
 systemctl disable postfix
 systemctl disable sshd
