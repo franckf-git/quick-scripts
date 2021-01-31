fb9d0ac97a6723744a288d534c004035031a0655
franckf <>
2021-01-31 16:49:17 +0100

    feat: pomodoro timer

---
+++ divers/pomodoro.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+clear
+echo "started - $(date)"
+
+short_loop () {
+    sleep 1m
+    mpv /usr/share/sounds/gnome/default/alerts/drip.ogg >> /dev/null 2>&1
+    echo "take a break - look away - 30 sec"
+    sleep 30s
+    mpv /usr/share/sounds/gnome/default/alerts/sonar.ogg >> /dev/null 2>&1
+    echo "go back to work"
+}
+
+while true
+do
+    # 1° short loop
+    short_loop
+    # 2° short loop
+    short_loop
+    # 3° short loop
+    short_loop
+    # 4° short loop
+    short_loop
+    # long loop
+    sleep 5m
+    mpv /usr/share/sounds/gnome/default/alerts/glass.ogg >> /dev/null 2>&1
+    echo "take a long break - stretch - 5 min"
+    sleep 5m
+    mpv /usr/share/sounds/gnome/default/alerts/sonar.ogg >> /dev/null 2>&1
+    echo "go back to work"
+done
+
