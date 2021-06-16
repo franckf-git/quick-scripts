af4e926f0f9940fce913c82f5a66c2aaf41dea11
franckf <>
2021-06-16 07:32:39 +0200

    feat: update apps - fix rpm versions

--- postinstalls/centos.sh
+++ postinstalls/centos.sh
@@ -36,20 +36,43 @@ flatpak remote-add --if-not-exists flathub     https://flathub.org/repo/flathub.
 # epel
 dnf install --assumeyes epel-release
 # rpm fusion
-dnf install --nogpgcheck --assumeyes https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm
-dnf install --nogpgcheck --assumeyes https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
+dnf install --assumeyes https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %centos).noarch.rpm
+dnf install --assumeyes https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %centos).noarch.rpm
 dnf config-manager --enable powertools
 
 # if minimal install
 # dnf install gnome-shell gdm gnome-tweaks nautilus gnome-terminal
 # systemctl set-default graphical.target
 
-dnf upgrade --assumeyes
+dnf upgrade --assumeyes --refresh
+# basics tools
+dnf install --assumeyes git rsync neovim #prename
+# softwares
+dnf install --assumeyes keepassxc #chromium newsboat urlview
+# file manager
+dnf install --assumeyes w3m-img ranger xclip highlight
+# medias
+dnf install --assumeyes mpv youtube-dl ffmpeg
+# code
+dnf install --assumeyes nodejs golang
+# systems
+dnf install --assumeyes tuned fira-code-fonts
+# if AMD (thinkpad)
+#dnf install --assumeyes xorg-x11-drv-amdgpu
+
+# power mangement
+tuned --daemon --profile powersave
+tuned-adm active
+tuned-adm verify
+
+# grub
+sed --in-place 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub
+grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
 
 # to disable wait for workspace login
 # systemctl disable NetworkManager-wait-online.service
 
 ############ Newsboat
-podman run --name=newsboat --detach --interactive --tty --volume ~/newsboat:/root:Z registry.fedoraproject.org/f34/fedora-toolbox
-podman exec --interactive --tty newsboat newsboat
+#podman run --name=newsboat --detach --interactive --tty --volume ~/newsboat:/root:Z registry.fedoraproject.org/f34/fedora-toolbox
+#podman exec --interactive --tty newsboat newsboat
 # to fix : urlview dl in ~/ ; scripts site>rss anim/scan
